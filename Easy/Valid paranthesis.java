69dc837c4f6505fa876a38bf2e1554457a5b1f35
JEEVITHA2855 <jeevitha.shakthi.05gmail.com>
2025-07-08 14:26:02 +0530

    add

---
+++ Easy/Valid paranthesis.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> x = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '{' || c == '[' || c == '(') {
+                x.push(c);
+            } else {
+                if (x.isEmpty()) return false;
+
+                char top = x.pop();
+                if ((c==')'&&top!='(')||
+                    (c==']'&&top!='[')||
+                    (c=='}'&&top!='{')) {
+                    return false;
+                }
+            }
+        }
+
+        return x.isEmpty();
+    }
+}
