c1c15394ff3a0a0f312d1fb5f20110015ced3786
Jeevitha S <147826996+JEEVITHA2855@users.noreply.github.com>
2025-07-11 14:42:56 +0530

    solved leetcode

---
+++ Medium/622. Design Circular Queue.java
@@ -0,0 +1,52 @@
+class MyCircularQueue {
+        int size;
+        int cap;
+        int a[];
+        int rear;
+        int front;
+    public MyCircularQueue(int k) {
+        this.cap = k;
+        a = new int[cap];
+        size=0;
+        rear=-1;
+        front=0;
+    }
+    
+    public boolean enQueue(int value) {
+        if(isFull())
+            return false;
+        rear = (rear+1)%cap;
+        a[rear] = value;
+        size++;
+        return true;
+    }
+    
+    public boolean deQueue() {
+        if(isEmpty())
+            return false;
+        int value = a[front];
+        front = (front+1)%cap;
+        size--;
+        return true;
+    }
+    
+    public int Front() {
+        if(isEmpty())
+            return -1;
+        return a[front];
+    }
+    
+    public int Rear() {
+        if(isEmpty())
+            return -1;
+        return a[rear];
+    }
+    
+    public boolean isEmpty() {
+        return size==0;
+    }
+    
+    public boolean isFull() {
+        return size==cap;
+    }
+}
