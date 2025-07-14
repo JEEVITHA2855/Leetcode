8b02e00ccc305ce10a6db3df83cb9744de98c95a
Jeevitha S <147826996+JEEVITHA2855@users.noreply.github.com>
2025-07-14 21:52:42 +0530

    Updated

--- Easy/144. Binary Tree Preorder Traversal.java
+++ Easy/144. Binary Tree Preorder Traversal.java
@@ -2,7 +2,7 @@ class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> preorder = new ArrayList<>();
         if(root == null) return preorder; 
-        preorder.add(root.val); 
+        preorder.add(root.val);  
         preorder.addAll(preorderTraversal(root.left)); 
         preorder.addAll(preorderTraversal(root.right));
         return preorder;
