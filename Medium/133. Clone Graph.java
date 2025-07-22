8a2c310ef13c8d185ddcc25c2603e1eb259f83d4
Jeevitha S <147826996+JEEVITHA2855@users.noreply.github.com>
2025-07-22 14:57:45 +0530

    Updated Leetcode problems

---
+++ Medium/133. Clone Graph.java
@@ -0,0 +1,21 @@
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node==null) return null;
+
+        Map<Node,Node> x = new HashMap<>();
+        return dfs(node,x);
+    }
+
+    private Node dfs(Node cur,Map<Node,Node> x) {
+        if(x.containsKey(cur)) return x.get(cur);
+
+        Node copy=new Node(cur.val);
+        x.put(cur,copy);
+
+        for(Node nbr:cur.neighbors) {
+            copy.neighbors.add(dfs(nbr,x));
+        }
+
+        return copy;
+    }
+}
