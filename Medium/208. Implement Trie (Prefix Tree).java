d5d03418be5ef1ff630b5dd0bba2c6a5de19b933
Jeevitha S <147826996+JEEVITHA2855@users.noreply.github.com>
2025-07-29 21:32:28 +0530

    Updated

---
+++ Medium/208. Implement Trie (Prefix Tree).java
@@ -0,0 +1,42 @@
+class TrieNode {
+    TrieNode[] child=new TrieNode[26];
+    boolean isEnd=false;
+}
+
+class Trie{
+    private TrieNode root;
+
+    public Trie(){
+        root=new TrieNode();
+    }
+    public void insert(String word){
+        TrieNode node=root;
+        for (char ch:word.toCharArray()){
+            int ind=ch-'a';
+            if(node.child[ind]==null)
+                node.child[ind]=new TrieNode();
+            node=node.child[ind];
+        }
+        node.isEnd=true;
+    }
+    public boolean search(String word) {
+        TrieNode node=root;
+        for (char ch:word.toCharArray()){
+            int ind=ch-'a';
+            if (node.child[ind]==null)
+                return false;
+            node=node.child[ind];
+        }
+        return node.isEnd;
+    }
+    public boolean startsWith(String prefix) {
+        TrieNode node=root;
+        for(char ch:prefix.toCharArray()){
+            int ind=ch-'a';
+            if(node.child[ind]==null)
+                return false;
+            node=node.child[ind];
+        }
+        return true;
+    }
+}
