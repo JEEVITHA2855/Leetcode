70e452f6875d4332049a374fbc95770131087b45
Jeevitha S <147826996+JEEVITHA2855@users.noreply.github.com>
2025-08-01 21:13:20 +0530

    Updated

--- Easy/118. Pascal's Triangle.java
+++ Easy/118. Pascal's Triangle.java
@@ -1,6 +1,7 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> res = new ArrayList<>();
+
         
         for (int i=0;i<numRows;i++){
             List<Integer> row= new ArrayList<>();
