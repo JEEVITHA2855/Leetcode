534d4b7502a37c7d71e13534acef26d31aabe987
Jeevitha S <147826996+JEEVITHA2855@users.noreply.github.com>
2025-08-01 21:12:38 +0530

    Updated

--- Easy/118. Pascal's Triangle.java
+++ Easy/118. Pascal's Triangle.java
@@ -4,7 +4,7 @@ class Solution {
         
         for (int i=0;i<numRows;i++){
             List<Integer> row= new ArrayList<>();
-            row.add(1); 
+            row.add(1);
         
         for(int j=1;j<i;j++){
            int val=res.get(i-1).get(j-1)+res.get(i-1).get(j);
