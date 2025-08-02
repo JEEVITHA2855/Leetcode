3168bdaf6ba64ef770911d5fc327a390d0a6f91f
Jeevitha S <147826996+JEEVITHA2855@users.noreply.github.com>
2025-08-02 21:28:13 +0530

    Updated

---
+++ Medium/122. Best Time to Buy and Sell Stock II.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min=Integer.MAX_VALUE;
+       int sum=0,prof=0;
+       for (int i=0;i<prices.length-1;i++){
+        if(prices[i+1]>prices[i]){
+            prof=prices[i+1]-prices[i];
+            sum=sum+prof;
+        }
+       }
+            return sum;
+    }
+}
