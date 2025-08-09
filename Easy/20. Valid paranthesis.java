6a58a6aa6c7f09db3d3ec80e4b8cd360b1d8d845
Jeevitha S <147826996+JEEVITHA2855@users.noreply.github.com>
2025-08-09 22:57:10 +0530

    Update 20. Valid paranthesis.java

--- Easy/20. Valid paranthesis.java
+++ Easy/20. Valid paranthesis.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> x = new Stack<>();
-
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
 
