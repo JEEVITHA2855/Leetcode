edb074f42550035f2e68e6dcc0dec57eab2a54df
Jeevitha S <147826996+JEEVITHA2855@users.noreply.github.com>
2025-09-11 23:23:03 +0530

    Update

--- Easy/1. Two sum.java
+++ Easy/1. Two sum.java
@@ -1,4 +1,3 @@
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
     Map <Integer,Integer> map = new HashMap<>();
@@ -14,6 +13,7 @@ class Solution {
     return new int[]{-1,-1};
     }
 }
+
 
 
 
