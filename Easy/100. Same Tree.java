cc0119952fa57906b9b98885360844c76bc51172
Jeevitha S <147826996+JEEVITHA2855@users.noreply.github.com>
2025-10-24 21:59:32 +0530

    Fix logical operators in isSameTree method

--- Easy/100. Same Tree.java
+++ Easy/100. Same Tree.java
@@ -4,6 +4,7 @@ class Solution {
         if( p == null && q==null)
         return true;
         else if ((p==null && q!=null) || (p!=null && q==null))
+            
         return false;
 
         return ( (p.val == q.val) && isSameTree(p.left,q.left) && isSameTree(p.right ,q.right) ) ;
