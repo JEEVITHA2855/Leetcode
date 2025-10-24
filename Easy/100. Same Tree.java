799d50c8abb725a918eb94b273107701d6c35fa6
Jeevitha S <147826996+JEEVITHA2855@users.noreply.github.com>
2025-10-24 22:00:26 +0530

    Implement null checks in isSameTree method
    Added null checks for tree nodes in isSameTree method.

--- Easy/100. Same Tree.java
+++ Easy/100. Same Tree.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
 
+        
         if( p == null && q==null)
         return true;
         else if ((p==null && q!=null) || (p!=null && q==null))
