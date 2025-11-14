3a527a2b3b408d6ad609f9b0fa3de23fa1b8faa2
Jeevitha S <147826996+JEEVITHA2855@users.noreply.github.com>
2025-11-14 20:45:39 +0530

    Update 1004. Max Consecutive Ones III.java

--- Medium/1004. Max Consecutive Ones III.java
+++ Medium/1004. Max Consecutive Ones III.java
@@ -1,22 +1,17 @@
-class Solution {
-    public int longestOnes(int[] nums, int k) {
-        int left=0,maxLen=0,zeroC=0;
+class Solution{
+public int longestOnes(int[] nums, int k) {
+    int left=0,zeroCount=0,maxLen=0;
 
-        for (int right=0;right<nums.length;right++) {
-            if (nums[right]==0) {
-                zeroC++;
-            }
+    for (int i=0;i<nums.length;i++) {
+        if(nums[i] ==0) zeroCount++;
 
-            while (zeroC>k) {
-                if (nums[left]==0) {
-                    zeroC--;
-                }
-                left++;
-            }
-
-            maxLen = Math.max(maxLen,right-left+ 1);
+        while (zeroCount>k) {
+            if(nums[left]==0) zeroCount--;
+            left++;
         }
 
-        return maxLen;
+        maxLen=Math.max(maxLen,i-left+1);
     }
+    return maxLen;
+}
 }
